ed27ae58366958a18cfff032d3215037e57cb6f1
elmhamdi <soulefmhamdi2@gmail.com>
2020-01-13 11:21:13 +0100

    4 files modified, 16 added

---
+++ src/app/crudfunding/crudfunding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crudfunding',
+  templateUrl: './crudfunding.component.html',
+  styleUrls: ['./crudfunding.component.css']
+})
+export class CrudfundingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
